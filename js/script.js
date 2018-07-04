a86da701e21ba17843a3d4edd924c2f311d7fede
bazikk <33804374+bazikk@users.noreply.github.com>
2018-07-04 23:16:28 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict"
+
+const slider = document.querySelector(".dogs_shelter_slider");
+const modalFrame = document.querySelectorAll(".modal_frame");
+
+slider.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const target = e.target;
+    if(!target.classList.contains("btn")) return false;
+
+    for(let i of modalFrame){
+        if(i.getAttribute("data-modal") == target.closest(".slider_item").getAttribute("data-item")){
+            i.classList.toggle("showOn");
+            i.classList.remove("showOff");
+        }
+    }
+});
+for(let i of modalFrame){
+    i.querySelector(".close").addEventListener("click", (e)=>{
+        if(i.classList.contains("showOn")){
+            i.classList.toggle("showOff");
+            i.classList.remove("showOn");
+        }
+    })
+}
+
+window.addEventListener("resize" , (e)=>{
+    const windowWidth = document.documentElement.offsetWidth;
+    const btns = slider.querySelectorAll(".btn");
+    
+    if(parseInt(windowWidth) < 740){
+        for(let i of btns){
+            i.textContent = "More";
+        }
+    }else{
+        for(let i of btns){
+            i.textContent = "Learn More";
+        }
+    }
+})
---
+++ js/slider.js
@@ -0,0 +1,47 @@
+"use strict"
+//const slider = document.querySelector(".dogs_shelter_slider");
+const sliderItemsContainer = slider.querySelector(".slider_items_container");
+let position = 0;
+
+
+slider.querySelector(".prev").onclick = ()=>{
+    prevItem();
+}
+
+slider.querySelector(".next").onclick = ()=>{
+    nextItem();
+}
+
+function nextItem(){
+    const sliderItems = sliderItemsCount(slider);
+    const itemWidth = sliderItemWidth(sliderItems);
+
+    position = Math.max(position - itemWidth, -itemWidth * (sliderItems.length - 3));
+    sliderItemsContainer.style.marginLeft = `${(position* 100)/sliderItemsContainer.offsetWidth}%`;
+}
+
+function prevItem(){
+    const sliderItems = sliderItemsCount(slider);
+    const itemWidth = sliderItemWidth(sliderItems);
+
+    position = Math.min(position + itemWidth, 0);
+    sliderItemsContainer.style.marginLeft = `${(position* 100)/sliderItemsContainer.offsetWidth}%`;
+}
+
+function sliderItemsCount(slider){
+    const sliderItems = slider.querySelectorAll(".slider_item");
+    
+	return sliderItems;
+}
+
+function sliderItemWidth(sliderItemsCount){
+    const sliderItemStyle = getComputedStyle(sliderItemsCount[0]);
+    const itemWidth = sliderItemsCount[0].offsetWidth + parseInt(sliderItemStyle.marginRight) + parseInt(sliderItemStyle.marginLeft);
+                          
+    return itemWidth;
+  }
+
+function textFunc(){
+    return null;
+}
+
