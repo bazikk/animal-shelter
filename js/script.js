3be325f44a3574fa00092f15f0e7079b55e2bf44
bazikk <33804374+bazikk@users.noreply.github.com>
2018-07-09 21:40:05 +0300

    Added overlay and improved the modal window

--- js/script.js
+++ js/script.js
@@ -12,19 +12,39 @@ slider.addEventListener("click", (e)=>{
         if(i.getAttribute("data-modal") == target.closest(".slider_item").getAttribute("data-item")){
             i.classList.toggle("showOn");
             i.classList.remove("showOff");
+            document.body.classList.add("overlay");
         }
     }
+
+    
 });
+
 for(let i of modalFrame){
     i.querySelector(".close").addEventListener("click", (e)=>{
         if(i.classList.contains("showOn")){
             i.classList.toggle("showOff");
             i.classList.remove("showOn");
+            document.body.classList.remove("overlay");
         }
     })
 }
 
-window.addEventListener("resize" , (e)=>{
+// window.addEventListener("resize" , (e)=>{
+//     const windowWidth = document.documentElement.offsetWidth;
+//     const btns = slider.querySelectorAll(".btn");
+    
+//     if(parseInt(windowWidth) < 740){
+//         for(let i of btns){
+//             i.textContent = "More";
+//         }
+//     }else{
+//         for(let i of btns){
+//             i.textContent = "Learn More";
+//         }
+//     }
+// })
+
+function btnText(){
     const windowWidth = document.documentElement.offsetWidth;
     const btns = slider.querySelectorAll(".btn");
     
@@ -32,9 +52,7 @@ window.addEventListener("resize" , (e)=>{
         for(let i of btns){
             i.textContent = "More";
         }
-    }else{
-        for(let i of btns){
-            i.textContent = "Learn More";
-        }
     }
-})
+}
+
+btnText();
